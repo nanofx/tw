04f444fe1557ee3a11035640bfaec92ec962e2a9
nanofx <nanofx01@gmail.com>
2020-08-19 22:15:45 +0300

    Update index.js

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { storiesOf, addParameters } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
-import { action } from '@storybook/addon-actions'
+//import { action } from '@storybook/addon-actions'
 // import { linkTo } from '@storybook/addon-links'
 import { backgrounds } from './backgrounds'
 import './story.css'
@@ -77,59 +77,6 @@ storiesOf('Twitter Timeline Embed', module)
             options={{ height: 400}}
           />
         </div>
-      		        <div className='selfCenter spaceBetween standardWidth'>
-          <TwitterTimelineEmbed
-            sourceType={'timeline'}
-            userId={1429897651}
-            linkColor={'#4CAF50'}
-            theme={'light'}
-            options={{ height: 400}}
-          />
-        </div>
-      		        <div className='selfCenter spaceBetween standardWidth'>
-          <TwitterTimelineEmbed
-            sourceType={'timeline'}
-            userId={1429897651}
-            linkColor={'#4CAF50'}
-            theme={'light'}
-            options={{ height: 400}}
-          />
-        </div>
-      		        <div className='selfCenter spaceBetween standardWidth'>
-          <TwitterTimelineEmbed
-            sourceType={'timeline'}
-            userId={1429897651}
-            linkColor={'#4CAF50'}
-            theme={'light'}
-            options={{ height: 400}}
-          />
-        </div>
-      		        <div className='selfCenter spaceBetween standardWidth'>
-          <TwitterTimelineEmbed
-            sourceType={'timeline'}
-            userId={1429897651}
-            linkColor={'#4CAF50'}
-            theme={'light'}
-            options={{ height: 400}}
-          />
-        </div>
-      		        <div className='selfCenter spaceBetween standardWidth'>
-          <TwitterTimelineEmbed
-            sourceType={'timeline'}
-            userId={1429897651}
-            linkColor={'#4CAF50'}
-            theme={'light'}
-            options={{ height: 400}}
-          />
-        </div>
-      		        <div className='selfCenter spaceBetween standardWidth'>
-          <TwitterTimelineEmbed
-            sourceType={'timeline'}
-            userId={1429897651}
-            linkColor={'#4CAF50'}
-            theme={'light'}
-            options={{ height: 400}}
-          />
-        </div>
       </div>
     ))
+  )
