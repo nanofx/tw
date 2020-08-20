c8ef0e099c4447f72a4901ede0cd12acc3969871
nanofx <nanofx01@gmail.com>
2020-08-20 08:38:06 +0300

    Update index.js

--- src/stories/index.js
+++ src/stories/index.js
@@ -25,19 +25,106 @@ addParameters({
 })
 
 storiesOf('Twitter Timeline Embed', module)
-  .add(
-    'Timeline url (with profile url)',
+.add(
+    'Timeline url (with  url)',
     withInfo({
-      text: 'Timeline url example with given profile url'
+      text: 'Timeline url '
     })(() => (
       <div className='centerContent'>
-        <div className='selfCenter standardWidth'>
+        <div className='selfCenter standardWidth' style={{ width: 350, height: 350, margin: 20 }}>		
           <TwitterTimelineEmbed
             sourceType={'url'}
             url={'https://twitter.com/delfilietuva'}
-            options={{ height: 400 }}
+            autoHeight
+          />
+        </div>
+		  <div className='selfCenter standardWidth' style={{ width: 350, height: 350, margin: 20 }}>
+          <TwitterTimelineEmbed
+            sourceType={'url'}
+            url={'https://twitter.com/DelfiLV'}
+            autoHeight
+          />
+        </div>
+		<div className='selfCenter standardWidth' style={{ width: 350, height: 350, margin: 20 }}>
+          <TwitterTimelineEmbed
+            sourceType={'url'}
+            url={'https://twitter.com/15minlt'}
+            autoHeight
+          />
+        </div>
+				<div className='selfCenter standardWidth' style={{ width: 350, height: 350, margin: 20 }}>
+          <TwitterTimelineEmbed
+            sourceType={'url'}
+            url={'https://twitter.com/GitanasNauseda'}
+            autoHeight
+          />
+        </div>
+				<div className='selfCenter standardWidth' style={{ width: 350, height: 350, margin: 20 }}>
+          <TwitterTimelineEmbed
+            sourceType={'url'}
+            url={'https://twitter.com/Grybauskaite_LT'}
+            autoHeight
+          />
+        </div>
+				<div className='selfCenter standardWidth' style={{ width: 350, height: 350, margin: 20 }}>
+          <TwitterTimelineEmbed
+            sourceType={'url'}
+            url={'https://twitter.com/Skvernelis_S'}
+            autoHeight
           />
         </div>
       </div>
     ))
   )
+.add(
+    'Timeline url (with likes url)',
+    withInfo({
+      text: 'Timeline url example with given likes url'
+    })(() => (
+      <div className='centerContent'>
+        <div className='selfCenter standardWidth' style={{ width: 350, height: 350, margin: 20 }}>		
+          <TwitterTimelineEmbed
+            sourceType={'url'}
+            url={'https://twitter.com/delfilietuva/likes'}
+            autoHeight
+          />
+        </div>
+		  <div className='selfCenter standardWidth' style={{ width: 350, height: 350, margin: 20 }}>
+          <TwitterTimelineEmbed
+            sourceType={'url'}
+            url={'https://twitter.com/DelfiLV/likes'}
+            autoHeight
+          />
+        </div>
+		<div className='selfCenter standardWidth' style={{ width: 350, height: 350, margin: 20 }}>
+          <TwitterTimelineEmbed
+            sourceType={'url'}
+            url={'https://twitter.com/15minlt/likes'}
+            autoHeight
+          />
+        </div>
+				<div className='selfCenter standardWidth' style={{ width: 350, height: 350, margin: 20 }}>
+          <TwitterTimelineEmbed
+            sourceType={'url'}
+            url={'https://twitter.com/GitanasNauseda/likes'}
+            autoHeight
+          />
+        </div>
+				<div className='selfCenter standardWidth' style={{ width: 350, height: 350, margin: 20 }}>
+          <TwitterTimelineEmbed
+            sourceType={'url'}
+            url={'https://twitter.com/Grybauskaite_LT/likes'}
+            autoHeight
+          />
+        </div>
+				<div className='selfCenter standardWidth' style={{ width: 350, height: 350, margin: 20 }}>
+          <TwitterTimelineEmbed
+            sourceType={'url'}
+            url={'https://twitter.com/Skvernelis_S/likes'}
+            autoHeight
+          />
+        </div>
+      </div>
+    ))
+  )
+  
